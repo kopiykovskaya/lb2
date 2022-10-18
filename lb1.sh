36c851bfa1c0de20ba842c2e23e1f726dd6f3de2
kopiykovskaya <kopiykovskaya@gmail.com>
2022-10-18 21:13:46 +0300

    first commit

---
+++ lb1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source=/home/kopiykovskay/Documents/source
+dest=/home/kopiykovskay/Documents/destination
+
+for file in $(find $source -printf "%P\n") ; do
+    if [ -a $dest/$file ] ; then
+        if [ $source/$file -nt $dest/$file ] ; then
+        echo "Newer file detected, copying..."
+        cp -r $source/$file $dest/$file 
+        else 
+        echo "File $file exists, skipping. "
+        fi
+        else 
+        echo "$file is being copied over to $dest"
+        cp -r $source/$file $dest/$file
+        fi
+        done
+    
+    
